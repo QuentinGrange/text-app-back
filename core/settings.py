0b0a45478f88b8be64b928bc3358fa44cb098318
quentingrange <quentin.grange.5@gmail.com>
2022-06-15 18:30:34 +0200

    add django.setup() in asgi.py

--- core/settings.py
+++ core/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
     'channels',
     'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
