58146ec52d3f2bbf2aaf2d698f0558ccde3f9f02
quentingrange <quentin.grange.5@gmail.com>
2022-06-15 18:03:59 +0200

    fix ASGI_APPLICATION

--- core/asgi.py
+++ core/asgi.py
@@ -1,29 +1,35 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
+# """
+# ASGI config for core project.
+#
+# It exposes the ASGI callable as a module-level variable named ``application``.
+#
+# For more information on this file, see
+# https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+# """
+#
+# import os
+#
+# import django
+#
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# import api.routing
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+# django.setup()
+#
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             api.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
 
 import os
-
-import django
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import api.routing
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            api.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_default_application()
